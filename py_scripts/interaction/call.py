c2be5ac38ab65a772c752e76c73978d1744d1ba4
Alin Cruceat <alyn_509@yahoo.com>
2025-05-15 10:10:47 +0300

    python script update + tests done

--- py_scripts/interaction/call.py
+++ py_scripts/interaction/call.py
@@ -21,7 +21,6 @@ def main(cli_args: List[str]):
     parser.add_argument("--transfer-amount", type=int, default=0)
     args = parser.parse_args(cli_args)
 
-    caller
     match args.caller:
         case "owner":
             caller = context.deployer_account
@@ -45,9 +44,9 @@ def main(cli_args: List[str]):
             print("invalid account")
             return
         
-    context.deploy_or_load_sc_address(config.INIT_APY)
     context.call(caller, args.endpoint, args.transfer_amount)
     
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
